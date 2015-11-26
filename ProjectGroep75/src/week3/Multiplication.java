b02a757385f40b39922fb9aaaa54febf1ae302c2
Stan Peters <stan.peters@live.nl>
2015-11-26 14:12:42 +0100

    Allez

--- ProjectGroep75/src/week3/Multiplication.java
+++ ProjectGroep75/src/week3/Multiplication.java
@@ -1,14 +1,14 @@
 package week3;
 
-public class Multiplication implements OperatorWithIdentity {
-	private Multiplication mult;
+public class Multiplication implements OperatorWithIdentity{
 	private int result;
 	
-	public Multiplication(int first, int second){
-		mult = new Multiplication(first, second);
-		mult.result = first * second;
+	public int operate (int left, int right){
+		result = left * right;
+		return result;
 	}
-	public int result(){
-		return mult.result;
+	
+	public int identity (){
+		return 1;
 	}
 }
