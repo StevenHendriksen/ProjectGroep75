20bbaf54391b133a90c54ea4c8f8588b0044e1b0
Steven Hendriksen <Sjfhendriksen@gmail.com>
2015-11-27 00:46:01 +0100

    t/m 3.9 af, delen van 3.10 al verwerkt.

--- ProjectGroep75/src/week3/Hotel/Bill.java
+++ ProjectGroep75/src/week3/Hotel/Bill.java
@@ -1,17 +1,37 @@
 package week3.Hotel;
 
-public class Bill {
-	public item Bill.item;
-	public Bill(java.io.PrintStream theOutStream){
+public class Bill{
+	public interface Item {
+		public double getSum();
+//		public String ToString(String text, double amount);
+	}
+
+	private double sum;
+	java.io.PrintStream PS;
+	Format format;
+//	private String text = "Bill:\n";
+
+	public Bill(java.io.PrintStream theOutStream) {
+		PS = theOutStream;
+//		format = new Format(); 
 		
 	}
-	public void close(){
-		
+
+	public void close() {
+		PS.println(sum);
 	}
-	public double getSum(){
-		
+
+	public double getSum() {
+		return sum;
 	}
-	public void newItem(Bill.Item item){
-		
+
+	public void newItem(Item item) {
+		sum = sum + item.getSum();
+//		item.ToString(, amount))
 	}
+	/*
+	public void ToString(String text, double amount){
+		text = text + format.printLine(text, amount);
+	}
+	*/
 }
--- ProjectGroep75/src/week3/Hotel/Format.java
+++ ProjectGroep75/src/week3/Hotel/Format.java
@@ -1,24 +1,14 @@
 package week3.Hotel;
 
-import java.util.Formatter;
-
 public class Format {
 	String print;
 	Format format;
 
-	public Format() {
-		StringBuilder sb = new StringBuilder();
-
-	}
-
-	public void printLine(String text, double amount) {
+	public String printLine(String text, double amount) {
 		if(text.length() < 50 && amount < 10000){
-		System.out.print(String.format("%-50s%s%15s\n",text,"",amount));
+		return String.format("%-50s%s%15s\n",text,"",amount);
 		}
 		else
-			System.out.print(String.format("%-50s%s%15s\n","De tekst was te lang","",amount));
-				
-		
-
+			return String.format("%-50s%s%15s\n","De tekst was te lang","",amount);
 	}
 }
