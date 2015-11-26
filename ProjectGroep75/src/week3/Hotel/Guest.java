fd6d93d2f58adce857403342b94d9309c07ae775
Steven Hendriksen <Sjfhendriksen@gmail.com>
2015-11-26 22:15:09 +0100

    week3.Hotel added

---
+++ ProjectGroep75/src/week3/Hotel/Guest.java
@@ -0,0 +1,91 @@
+package week3.Hotel;
+
+
+/**
+ * Guests, perhaps with a room.
+ * @author Stan Peters en Steven Hendriksen
+ * @version $Revision: 1.0 $
+ */
+
+public class Guest {
+	
+	 // ------------------ Instance variables ----------------
+	
+	private String name;
+	private Room room;
+	
+	 // ------------------ Constructor ------------------------
+	
+    /**
+     * Creates a <code>Guest</code> with the given name, without a room.
+     * @param n is the name of the <code>Guest</code>
+     */
+	public Guest(java.lang.String n){
+		name = n;
+	}
+	
+    // ------------------ Queries --------------------------
+	
+    /**
+     * Returns the name of this <code>Guest</code>.
+     */
+	
+	
+	public java.lang.String getName(){
+		return name;
+	}
+	
+    /**
+     * Returns the current room of the <code>Guest</code>.
+     * @return the room of this <code>Guest</code>;
+     *         <code>null</code> if this <code>Guest</code> 
+     *         does not have a room.
+     */
+	
+	public Room getRoom(){
+		return room;
+		
+	}
+
+    // ------------------ Commands --------------------------
+	
+    /**
+     * Assigns a <code>Room</code> to this <code>Guest</code>.
+     * @param r the room that the <code>Guest</code> is renting;
+     *        if <code>null</code> is passed, this <code>Guest</code>
+     *        does not have a room.
+     */
+	
+	public boolean checkin(Room r){
+		if (room == null && r.getGuest() == null){
+			room = r;
+			r.setGuest(this);
+			return true;
+		}
+		else{
+			return false;
+		}
+
+	}
+	
+    /**
+     * Remove a <code>Room</code> from this <code>Guest</code>.
+     */
+	
+	public boolean checkout(){
+		if (room != null && room.getGuest() != null){
+			room.setGuest(null);
+			room = null;
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+
+	public java.lang.String toString(){
+		String summary = room + " " + name; 
+		return summary;
+	}
+}
---
+++ ProjectGroep75/src/week3/Hotel/Password.java
@@ -0,0 +1,85 @@
+package week3.Hotel;
+
+/**
+ * Passwords
+ * 
+ * @author Stan Peters en Steven Hendriksen
+ * @version $Revision: 1.0 $
+ */
+
+public class Password {
+
+	// ------------------ Instance variables ----------------
+	private String passw;
+
+	// ------------------ Initial ------------------------
+
+	/**
+	 * Creates a <code>INITIAL</code> with the string "123456".
+	 */
+	public static final String INITIAL = "123456";
+
+	// ------------------ Constructor ------------------------
+
+	/**
+	 * Creates a <code>Password</code> with the given initial.
+	 * 
+	 * @param passw
+	 *            is the name of the <code>Password</code>
+	 */
+	public Password() {
+		passw = INITIAL;
+	}
+
+	// ------------------ Queries --------------------------
+
+	/**
+	 * Shows whether the <code>Password</code> is acceptable or not.
+	 * 
+	 * @param suggestion
+	 *            the password which is given by a person;
+	 */
+	public boolean acceptable(String suggestion) {
+		if (suggestion.length() >= 6 && suggestion.contains(" ") == false) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Shows whether the <code>Password</code> is equal to the current
+	 * <code>Password</code>.
+	 * 
+	 * @param test
+	 *            the password which is given by a person;
+	 */
+	public boolean testWord(String test) {
+		if (test.equals(passw)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Set a new <code>Password</code> when the old password is the same as the
+	 * current and the new password is acceptable.
+	 * 
+	 * @param oldpass
+	 *            the current password which is given by a person;
+	 * @param newpass
+	 *            the new password which is given by a person;
+	 */
+	public boolean setWord(String oldpass, String newpass) {
+		if (oldpass.equals(passw) && acceptable(newpass)) {
+			passw = newpass;
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+}
+	
