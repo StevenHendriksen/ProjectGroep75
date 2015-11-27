f66e9b0604a584e8e68b95b4d4c01e49a0feca71
Stan Peters <stan.peters@live.nl>
2015-11-27 11:35:01 +0100

    Steven is een kale kanarie

--- ProjectGroep75/src/week3/pw/Checker.java
+++ ProjectGroep75/src/week3/pw/Checker.java
@@ -1,6 +1,16 @@
 package week3.pw;
 
+/**
+ * Checker
+ * 
+ * @author Stan Peters en Steven Hendriksen
+ * @version $Revision: 1.0 $
+ */
+
 public interface Checker {
+	/**
+	 * These methods are all in BasicChecker and StrongChecker
+	 */
 	public boolean acceptable(String password);
 	
 	public String generatePassword();
