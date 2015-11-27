2a959ae2e9bde0c3be01252e5880815441983439
Steven Hendriksen <Sjfhendriksen@gmail.com>
2015-11-27 11:20:35 +0100

    Basicchecker javadoc

--- ProjectGroep75/src/week3/pw/BasicChecker.java
+++ ProjectGroep75/src/week3/pw/BasicChecker.java
@@ -1,14 +1,33 @@
 package week3.pw;
 
+/**
+ * BasicChecker;
+ * 
+ * @author Stan Peters en Steven Hendriksen
+ * @version 1.0
+ */
+
 public class BasicChecker implements Checker {
+	// ------------------ Instance variables ----------------
 	private String passw;
 	
 	public static final String INITPASS = "A1234567890";
 	
+	// ------------------ Constructor ------------------------
+	/**
+	 * Creates a new password;
+	 * 
+	 * @param passw
+	 *            the new password;
+	 */	
 	public BasicChecker() {
 		passw = INITPASS;
 	}
-	
+	// ------------------ Commands ------------------------
+	/**
+	 * Tests if the password is acceptable;
+	 * returns boolean based on if it is acceptable
+	 */
 	public boolean acceptable(String password){
 		if (password.length() >= 6 && password.contains(" ") == false) {
 			return true;
@@ -16,6 +35,11 @@ public class BasicChecker implements Checker {
 			return false;
 		}
 	}
+	/**
+	 * Generate a password
+	 * @param passw
+	 * 				the generated password
+	 */
 	public String generatePassword(){
 		return passw;
 	}
