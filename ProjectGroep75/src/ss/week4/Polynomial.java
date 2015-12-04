46638b5ef911d4635c484e89730931997750a21c
Stan Peters <stan.peters@live.nl>
2015-12-04 11:18:48 +0100

    tm 4.9

--- ProjectGroep75/src/ss/week4/Polynomial.java
+++ ProjectGroep75/src/ss/week4/Polynomial.java
@@ -1,23 +1,41 @@
 package ss.week4;
 
 public class Polynomial implements Function{
-	LinearProduct[] array;
-	Constant constant1;
-	Function constant2;
+	private LinearProduct[] array;
+	private Constant newConstant;
+	private Exponent newExponent;
 	
 	public Polynomial(double[] as, double[] pows){
-		for(int i = 0; i == as.length; i++){
-			constant1 = new Constant(as[i]);
-			constant2 = new Constant(pows[i]);
-			array = new LinearProduct[constant1, constant2];
+		assert as.length == pows.length;
+		array = new LinearProduct[as.length];
+		for(int i = 0; i < as.length; i++){
+			newConstant = new Constant(as[i]);
+			newExponent = new Exponent((int) pows[i]);
+			array[i] = new LinearProduct(newConstant, newExponent);
 		}
 	}
 	
 	public double apply(int apply){
-		
+		double sum = 0;
+		for(int i = 0; i < array.length; i++){
+			double pow = Math.pow(apply, array[i].getFunction().apply(0));
+			double product = array[i].getConstant().apply(0) * pow;
+			sum += product;
+		}
+		return sum;
 	}
 	
-	public Function derivative(){
+	public Polynomial derivative(){
+		double[] as = new double[array.length];
+		double[] pows = new double[array.length];
 		
+		for(int i=0; i < array.length; i++){
+			as[i] = ((Exponent) (array[i].getFunction())).getPower() * array[i].getConstant().apply(0);
+			pows[i] = ((Exponent) (array[i].getFunction())).getPower() - 1;
+		}
+		
+		Polynomial pol = new Polynomial(as, pows);
+		
+		return pol;
 	}
 }
