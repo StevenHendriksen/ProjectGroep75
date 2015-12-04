37748049a5cd0339ab17798d57f05349d238ec5c
Stan Peters <stan.peters@live.nl>
2015-12-04 16:05:25 +0100

    Polynomial opnieuw, nu doet hij het wel. Samen met PolynomialTest.

--- ProjectGroep75/src/ss/week4/Polynomial.java
+++ ProjectGroep75/src/ss/week4/Polynomial.java
@@ -1,41 +1,37 @@
 package ss.week4;
 
-public class Polynomial implements Function{
+public class Polynomial implements Function {
 	private LinearProduct[] array;
 	private Constant newConstant;
 	private Exponent newExponent;
-	
-	public Polynomial(double[] as, double[] pows){
+
+	public Polynomial(double[] as, double[] pows) {
 		assert as.length == pows.length;
 		array = new LinearProduct[as.length];
-		for(int i = 0; i < as.length; i++){
-			newConstant = new Constant(as[i]);
-			newExponent = new Exponent((int) pows[i]);
-			array[i] = new LinearProduct(newConstant, newExponent);
+		for (int i = 0; i < as.length; i++) {
+			array[i] = new LinearProduct(new Constant(as[i]), new Exponent((int) pows[i]));
 		}
 	}
-	
-	public double apply(int apply){
+
+	public double apply(int apply) {
 		double sum = 0;
-		for(int i = 0; i < array.length; i++){
-			double pow = Math.pow(apply, array[i].getFunction().apply(0));
-			double product = array[i].getConstant().apply(0) * pow;
+		for (int i = 0; i < array.length; i++) {
+			double product = array[i].getConstant().apply(apply) * array[i].getFunction().apply(apply);
 			sum += product;
 		}
 		return sum;
 	}
-	
-	public Polynomial derivative(){
+
+	public Polynomial derivative() {
 		double[] as = new double[array.length];
 		double[] pows = new double[array.length];
-		
-		for(int i=0; i < array.length; i++){
+
+		for (int i = 0; i < array.length; i++) {
 			as[i] = ((Exponent) (array[i].getFunction())).getPower() * array[i].getConstant().apply(0);
 			pows[i] = ((Exponent) (array[i].getFunction())).getPower() - 1;
 		}
-		
+
 		Polynomial pol = new Polynomial(as, pows);
-		
 		return pol;
 	}
 }
