3119e974f96d07bfa679f6fac3ed005c6cc67897
Stan Peters <stan.peters@live.nl>
2015-12-11 12:06:17 +0100

    6421,

--- ProjectGroep75/src/ss/week5/MapUtil.java
+++ ProjectGroep75/src/ss/week5/MapUtil.java
@@ -6,38 +6,46 @@ import java.util.Map;
 import java.util.Set;
 
 public class MapUtil {
-	//@ requires map != null;
-	//@ ensures \result == true || \result == false;
-    public static <K, V> boolean isOneOnOne(Map<K, V> map) {
-    	for(int i = 1; i < map.values().size(); i++){
-       		for(int j = i; j < map.values().size(); j++){
-       			if(map.get(i) == map.get(j+1)){
-       	       		return false;
-       	       	}
-       		}
-       	}
-        return true;
-    }
-    
-    public static <K, V> 
-           boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
-        // TODO: implement, see exercise P-5.2
-        return false;
-    }
-    public static <K, V> Map<V, Set<K>> inverse(Map<K, V> map) {
-        // TODO: implement, see exercise P-5.3
-        return null;
+	// @ requires map != null;
+	// @ ensures \result == true || \result == false;
+	public static <K, V> boolean isOneOnOne(Map<K, V> map) {
+		for (int i = 1; i < map.values().size(); i++) {
+			for (int j = i; j < map.values().size(); j++) {
+				if (map.get(i) == map.get(j + 1)) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
+
+	public static <K, V> boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
+		for (V v : range) {
+			if (!map.containsValue(v)) {
+				return false;
+			}
+			
+		}
+		return true;
+	}
+
+	public static <K, V> Map<V, Set<K>> inverse(Map<K, V> map) {
+		
+		return null;
+	}
+
 	public static <K, V> Map<V, K> inverseBijection(Map<K, V> map) {
-        // TODO: implement, see exercise P-5.3
-        return null;
+		// TODO: implement, see exercise P-5.3
+		return null;
 	}
+
 	public static <K, V, W> boolean compatible(Map<K, V> f, Map<V, W> g) {
-        // TODO: implement, see exercise P-5.4
-        return false;
+		// TODO: implement, see exercise P-5.4
+		return false;
 	}
+
 	public static <K, V, W> Map<K, W> compose(Map<K, V> f, Map<V, W> g) {
-        // TODO: implement, see exercise P-5.5
-        return null;
+		// TODO: implement, see exercise P-5.5
+		return null;
 	}
 }
