c417b2eab1e3551f5e930a916729478600ec2ddc
Steven Hendriksen <Sjfhendriksen@gmail.com>
2015-12-15 11:16:56 +0100

    BREAK EVERYTHING

--- ProjectGroep75/src/ss/week6/Words.java
+++ ProjectGroep75/src/ss/week6/Words.java
@@ -2,10 +2,10 @@ package ss.week6;
 
 import java.util.Scanner;
 
-public class Hello {
-	public Hello(java.io.InputStream InputStream) {
-	}
+public class Words {
 
+	public Words() {
+	}
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		while (in.hasNextLine()) {
