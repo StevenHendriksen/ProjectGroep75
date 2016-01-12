db1b8d5354239a885411be3edc116278078b4743
Steven Hendriksen <Sjfhendriksen@gmail.com>
2016-01-12 12:22:22 +0100

    Qwirkle

---
+++ ProjectGroep75/Qwirkle/Client/Qwirkle.java
@@ -0,0 +1,15 @@
+package Client;
+
+/**
+ * 
+ * Executable client class for Qwirkle
+ * 
+ * @author Stan Peters en Steven Hendriksen
+ * @version $Revision: 1.0 $
+ */
+
+public class Qwirkle {
+	public static void main(String[] args){
+		
+	}
+}
