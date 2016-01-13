c6aeb688f807e34cd4eb4e1b9e14ac61d8ea5ff5
Stan Peters <stan.peters@live.nl>
2016-01-13 10:07:22 +0100

    Bag

--- ProjectGroep75/Qwirkle/Server/Bag.java
+++ ProjectGroep75/Qwirkle/Server/Bag.java
@@ -4,59 +4,100 @@ public class Bag {
 	private Cube[] cubes;
 	private Color color;
 	private Shape shape;
-	
-	public Bag(){
-		
+
+	public Bag() {
+
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		Bag bag = new Bag();
-		
+
 		bag.newBag();
-		bag.takeCube().printCube();
 	}
-	
-	public void newBag(){
+
+	public void newBag() {
 		cubes = new Cube[108];
 		color = Color.RED;
 		shape = Shape.CIRCLE;
-		for(int j = 0; j < 36; j++){
-			for(int i = 0; i < 3; i++){
-				cubes[i + j*3] = new Cube(color, shape);
+		for (int j = 0; j < 36; j++) {
+			for (int i = 0; i < 3; i++) {
+				cubes[i + j * 3] = new Cube(color, shape);
 			}
-			if(j % 6 == 0){
+			if (j % 6 == 0) {
 				shape = shape.other();
 			}
 			color = color.other();
 		}
 	}
-	
-	public Cube takeCube(){
+
+	public Cube takeCube() {
 		int random = (int) Math.floor(Math.random() * 108);
 		Cube randomcube = cubes[random];
-		
-		while(randomcube == null){
+
+		while (randomcube == null) {
 			random = (int) Math.floor(Math.random() * 108);
 		}
 
 		randomcube = null;
-		
+
 		return randomcube;
 	}
-	
-	public boolean emptyBag(){
-		boolean empty = true;
-		
+
+	public void getCube(Cube cube){
+		boolean inBag = false;
 		for(int i = 0; i < 108; i++){
-			if(cubes[i] != null){
+			if(cubes[i] == cube){
+				if(i % 3 == 0 && cubes[i + 1] == null){
+					cubes[i + 1] = cube;
+				}
+				else if(i % 3 == 0 && cubes[i + 2] == null){
+					cubes[i + 2] = cube;
+				}
+				else if(i % 3 == 1 && cubes[i - 1] == null){
+					cubes[i - 1] = cube;
+				}
+				else if(i % 3 == 1 && cubes[i + 1] == null){
+					cubes[i + 1] = cube;
+				}
+				else if(i % 3 == 2 && cubes[i - 2] == null){
+					cubes[i - 2] = cube;
+				}
+				else if(i % 3 == 2 && cubes[i - 1] == null){
+					cubes[i - 1] = cube;
+				}
+				inBag = true;
+			}
+		}
+		if (!inBag){
+			for(int i = 0; i < 108; i++){
+				if(cubes[i] == null){
+					cubes[i] = cube;
+				}
+			}
+		}
+	}
+
+	public Bag deepCopy() {
+		Bag bag = new Bag();
+		for (int i = 0; i < 108; i++) {
+			cubes[i] = this.cubes[i];
+		}
+		return bag;
+	}
+
+	public boolean emptyBag() {
+		boolean empty = true;
+
+		for (int i = 0; i < 108; i++) {
+			if (cubes[i] != null) {
 				empty = false;
 			}
 		}
-		
+
 		return empty;
 	}
-	
-	public Cube[] cubesInBag(){
+
+	public Cube[] cubesInBag() {
 		return cubes;
 	}
 }
