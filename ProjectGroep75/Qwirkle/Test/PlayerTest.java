74bdb6fed35ac159528b2e244a8fab5d08ce98fb
Steven Hendriksen <Sjfhendriksen@gmail.com>
2016-01-26 04:19:37 +0100

    constructor fix

--- ProjectGroep75/Qwirkle/Test/PlayerTest.java
+++ ProjectGroep75/Qwirkle/Test/PlayerTest.java
@@ -12,59 +12,61 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.PrintWriter;
+
 //af
 public class PlayerTest {
-	Player player;
-	Bag bag;
-	
-	@Before
-	public void setUp(){
-		bag = new Bag();
-    	player = new Player("Stan", bag);
+  Player player;
+  Bag bag;
+
+  @Before
+  public void setUp() {
+    bag = new Bag();
+    player = new Player("Stan", bag, new PrintWriter(System.out));
+  }
+
+  @Test
+  public void hasNameTest() {
+    assertEquals(player.hasName(), "Stan");
+  }
+
+  @Test
+  public void getTilesAndhasTilesTest() {
+    boolean test1 = true;
+
+    for (int i = 0; i < 6; i++) {
+      if (player.hasTiles()[i] != null) {
+        test1 = false;
+      }
     }
-	
-	@Test
-	public void hasNameTest(){
-		assertEquals(player.hasName(), "Stan");
-	}
-	
-	@Test
-	public void getTilesAndhasTilesTest(){
-		boolean test1 = true;
-		
-		for(int i = 0; i < 6; i++){
-			if(player.hasTiles()[i] != null){
-				test1 = false;
-			}
-		}
-		
-		assertTrue(test1);
-		
-		player.getTiles();
-		
-		for(int j = 0; j < 6; j++){
-			if(player.hasTiles()[j] == null){
-				test1 = false;
-			}
-		}
-		
-		assertTrue(test1);
-	}
-	
-	@Test
-	public void changeTilesTest(){
-		player.changeTiles(new Servertile(1), 0);
-		
-		assertTrue(player.hasTiles()[0].hasColor() == Color.RED);
-		assertTrue(player.hasTiles()[0].hasShape() == Shape.CIRCLE);
-	}
-	
-	@Test
-	public void hasScoreAndChangeScoreTest(){
-		assertTrue(player.hasScore() == 0);
-		
-		player.changeScore(5);
-		
-		assertTrue(player.hasScore() == 5);
-	}
+
+    assertTrue(test1);
+
+    player.getTiles();
+
+    for (int j = 0; j < 6; j++) {
+      if (player.hasTiles()[j] == null) {
+        test1 = false;
+      }
+    }
+
+    assertTrue(test1);
+  }
+
+  @Test
+  public void changeTilesTest() {
+    player.changeTiles(new Servertile(1), 0);
+
+    assertTrue(player.hasTiles()[0].hasColor() == Color.RED);
+    assertTrue(player.hasTiles()[0].hasShape() == Shape.CIRCLE);
+  }
+
+  @Test
+  public void hasScoreAndChangeScoreTest() {
+    assertTrue(player.hasScore() == 0);
+
+    player.changeScore(5);
+
+    assertTrue(player.hasScore() == 5);
+  }
 }
